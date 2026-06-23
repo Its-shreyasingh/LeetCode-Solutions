66e0c9769118a04483ba8a4797a5226017302a31
SHREYA SINGH <ssshreya1210@gmail.com>
2026-06-23 12:18:05 +0530

    Time: 0 ms (100%), Space: 15.5 MB (62.5%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(!head || !head->next || !head->next->next)
+        if(!head)
         {
             return head;
         }
