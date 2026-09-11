42424eae0f4f4d2a84f198eb7f16209b1acb892c
SHREYA SINGH <ssshreya1210@gmail.com>
2026-09-11 22:02:07 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.99%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> ms,mt;
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if((ms.find(s[i])!=ms.end()))
+            {
+                if(ms[s[i]] !=t[i])
+                {
+                    return false;
+                }
+            }
+            else if((mt.find(t[i])!=mt.end()))
+            {
+                if(mt[t[i]] !=s[i])
+                {
+                    return false;
+                }
+            }
+            else{
+                ms[s[i]]=t[i];
+                mt[t[i]]=s[i];
+            }
+        }
+        return true;
+    }
+};
